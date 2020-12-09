3851199e4a8a86bea29a36ad9245518890722f32
Alex <motzart66@gmail.com>
2020-12-09 11:39:41 +0200

    test cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 );
 
 const corsOptions = {
-  origin: 'https://staking.minerall.io/',
+  origin: 'https://example.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
